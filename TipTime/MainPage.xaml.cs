66590f8a50e3d6d53b3a4e1ebcf04692efbcf76f
AbreuAngl <angelomello538@gmail.com>
2025-09-01 22:32:23 -0300

    Aula TipTime - 01/09/25

---
+++ TipTime/MainPage.xaml.cs
@@ -0,0 +1,44 @@
+﻿namespace TipTime
+{
+    public partial class MainPage : ContentPage
+    {
+        int count = 0;
+
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+
+        private void Por15Btn_Clicked(object sender, EventArgs e)
+        {
+            PorcentagemSlider.Value = 15;
+        }
+
+        private void Por20Btn_Clicked(object sender, EventArgs e)
+        {
+            PorcentagemSlider.Value = 20;
+        }
+
+        private void ArredondaCimaBtn_Clicked(object sender, EventArgs e)
+        {
+
+        }
+
+        private void ArredondaBaixoBtn_Clicked(object sender, EventArgs e)
+        {
+
+        }
+
+        private void PorcentagemSlider_ValueChanged(object sender, ValueChangedEventArgs e)
+        {
+            double total = double.Parse(ValorTotalEntry.Text);
+            int porcentagem = int.Parse(PorcentagemSlider.Text);
+            int gorjeta = int.Parse(PorcentagemGorjeta.Text);
+            gorjeta = total * (porcentagem / 100);
+
+
+
+        }
+    }
+}
